9f7ff79950243d14b683e4decb1a6a0cf03c43a3
minckim <kmc6874@gmail.com>
2021-11-12 00:20:00 +0900

    #8 추가: 클라이언트 소켓 내용: 우선은 블로킹 소켓으로 만들었다.

--- Socket/SocketClient.cpp
+++ Socket/SocketClient.cpp
@@ -1,23 +1,57 @@
 #include "SocketClient.hpp"
 
-// //------------------------------------------------------------------------------
-// void		SocketClient::init()
-// {
-// 	sock = socket(PF_INET, SOCK_STREAM, 0);
-// 	if (sock == INVALID_SOCKET)
-// 	{
-// 		throw string("SocketFailed");
-// 		// throw SocketFailed();
-// 	}
-// 	memset(&addr, 0, sizeof(SOCKADDR_IN));
-// 	addr.sin_family = AF_INET;
-// }
-// //------------------------------------------------------------------------------
-// int			SocketClient::connect()
-// {
-// 	return ::connect(sock, reinterpret_cast<SOCKADDR*>(&addr_send), sizeof(SOCKADDR_IN));
-// }
-// //------------------------------------------------------------------------------
-// int			SocketClient::connect(const string& ip, int port)
-// {
-// }
+using namespace std;
+/*##############################################################################
+
+	Socket Client
+
+##############################################################################*/
+/*---------------------------------
+	Constructor & Destructor
+---------------------------------*/	
+SocketClient::SocketClient()
+:	__SocketBase()
+{}
+
+SocketClient::SocketClient(const std::string& ip, int port)
+:	__SocketBase()
+{
+	clear_addr();
+}
+
+
+
+//------------------------------------------------------------------------------
+void		SocketClient::init()
+{
+	sock = socket(PF_INET, SOCK_STREAM, 0);
+	if (sock == INVALID_SOCKET)
+	{
+		throw string("SocketFailed");
+		// throw SocketFailed();
+	}
+	memset(&addr, 0, sizeof(SOCKADDR_IN));
+	addr.sin_family = AF_INET;
+}
+//------------------------------------------------------------------------------
+int			SocketClient::connect()
+{
+	return ::connect(sock, reinterpret_cast<SOCKADDR*>(&addr), sizeof(SOCKADDR_IN));
+}
+//------------------------------------------------------------------------------
+int			SocketClient::connect(const string& ip, int port)
+{
+	set_ip(ip);
+	set_port(port);
+	return connect();
+}
+//------------------------------------------------------------------------------
+int			SocketClient::recv(void* buffer, int len)
+{
+	return ::recv(sock, reinterpret_cast<char*>(buffer), len, 0);
+}
+//------------------------------------------------------------------------------
+int			SocketClient::send(void* buffer, int len)
+{
+	return ::send(sock, reinterpret_cast<char*>(buffer), len, 0);
+}
--- Socket/SocketClient.hpp
+++ Socket/SocketClient.hpp
@@ -1,8 +1,31 @@
 #include "__SocketBase.hpp"
+#include <string>
+/*##############################################################################
 
+	Socket Client
+
+##############################################################################*/
 class SocketClient: public __SocketBase
 {
+	/*---------------------------------
+		Member
+	---------------------------------*/	
+	int			len_read_remain;
+	int			len_read;
+
+	/*---------------------------------
+		Constructor & Destructor
+	---------------------------------*/	
+	public:
+	SocketClient();
+	SocketClient(const std::string& ip, int port);
+
+	/*---------------------------------
+		Methods
+	---------------------------------*/	
 	void		init();
 	int			connect();
 	int			connect(const std::string& ip, int port);
+	int			recv(void* buffer, int len);
+	int			send(void* buffer, int len);
 };
