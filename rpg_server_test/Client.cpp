c00b49ce924d45fe04c7b7e09cf45cb8c9b6895d
minckim <kmc6874@gmail.com>
2021-11-13 17:19:38 +0900

    #23 추가: 혼자 움직는 것, 화면

--- rpg_server_test/Client.cpp
+++ rpg_server_test/Client.cpp
@@ -8,11 +8,15 @@ using namespace std;
 
 ##############################################################################*/
 Client::Client(const string& ip, int port)
-:	socket(ip, port), status(Status::START)
+:	socket(ip, port),
+	status(Status::START),
+	time_start(),
+	screen(SCREEN_SIZE)
 {}
 //------------------------------------------------------------------------------
 void		Client::run()
 {
+	cout << this << endl;
 	while (1)
 	{
 		switch (status)
@@ -28,8 +32,13 @@ void		Client::run()
 		case Status::GAME:
 			game();
 			break;
+
+		case Status::EXIT:
+			return;
+			break;
 		
 		default:
+			return;
 			break;
 		}
 	}
@@ -70,30 +79,99 @@ void		Client::controller(Message& message)
 //------------------------------------------------------------------------------
 void		Client::game()
 {
-	Message		message;
+	KeyManager	k_move(VK_UP);
+	KeyManager	k_turn_left(VK_LEFT);
+	KeyManager	k_turn_right(VK_RIGHT);
+	KeyManager	k_shot(VK_SPACE);
+	KeyManager	k_exit0('Q');
+	KeyManager	k_exit1(VK_ESCAPE);
 
+	FrameManager	screen_manager(100);
 
+	double		time0 = time_now();
+	uint32_t	bullet_count = 0;
 	while (1)
 	{
+		double	time_cur = time_now();
+
+		me->movable.speed = 0;
 		// instruction
+		double	time1 = time_now();
+		if (k_turn_left.is_pressed())
+			me->movable.rotate((time_cur - time0) / 1000);
+		if (k_turn_right.is_pressed())
+			me->movable.rotate(-(time_cur - time0) / 1000);
+		if (k_move.is_pressed())
+			me->movable.speed = me->speed;
+		if (k_shot.is_pressed())
+		{
+			shot(bullet_count);
+		}
+		if (k_exit0.is_pressed() || k_exit1.is_pressed())
+		{
+			status = Status::EXIT;
+			return;
+		}
 
-		// update
+		update(time_cur - time0);
+		time0 = time_cur;
 
-		// send message
-		// screen
+
+
+		screen_manager.set_interval(*this, PrintScreen());
 	}
 }
+//------------------------------------------------------------------------------
+void		Client::shot(uint32_t bullet_count)
+{
+	// uint32_t	bullet_id = me->id * 0x1000 + bullet_count++;
+	// bullets[bullet_id] = BulletBase();
+	// BulletBase*	ptr_bullet = &bullets[bullet_id];
+	// my_bullets.push_back(ptr_bullet);
+	// ptr_bullet->id = bullet_id;
+	// ptr_bullet->movable = me->movable;
+	// ptr_bullet->movable.speed = SPEED_BULLET;
+}
+//------------------------------------------------------------------------------
+void		Client::update(double time)
+{
+	// vector<BulletBase*>		to_be_erased;
+	// to_be_erased.reserve(my_bullets.size());
+
+	// for (BulletBase* ptr_bullet : my_bullets)
+	// {
+	// 	for (pair<const uint32_t, PlayerBase>& player_pair : players)
+	// 	{
+	// 		PlayerBase&	player = player_pair.second;
+	// 		if (&player == me)
+	// 			continue;
+	// 		if (ptr_bullet->movable.is_collide(player.movable, time, 1))
+	// 		{
+	// 			to_be_erased.push_back(ptr_bullet);
+	// 			player.id_alive = false;
+	// 		}
+	// 	}
+	// }
+
+	for (pair<const uint32_t, PlayerBase>& player_pair : players)
+	{
+		PlayerBase&	player = player_pair.second;
+		player.movable.update(time);
+	}
+}
+
 //------------------------------------------------------------------------------
 void		Client::service_connect(ResConnect& res)
 {
 	if (res.result == ResConnect::LOGIN_SUCCESS)
 	{
+		players[res.player.id] = res.player;
+		me = &players[res.player.id];
+		
 		cout << "Login Success" << endl;
 		cout << "ID: " << res.player.id << endl;
 		cout << "shape: " << res.player.shape << endl;
-		
-		players[res.player.id] = res.player;
-		me = &players[res.player.id];
+		cout << "name: " << res.player.name << endl;
 		
 		status = Status::GAME;
 	}
@@ -140,3 +218,29 @@ void		Client::login_wait()
 }
 
 //------------------------------------------------------------------------------
+double			Client::time_now()
+{
+	return static_cast<double>(chrono::duration_cast<chrono::microseconds>
+		(clock::now() - time_start).count()) / 1000;
+}
+//------------------------------------------------------------------------------
+void			Client::operator()(PrintScreen dummy)
+{
+	screen.clear_buffer();
+	for (pair<const uint32_t, PlayerBase>& player_pair : players)
+	{
+		PlayerBase&	player = player_pair.second;
+		screen.draw_point(player.movable.position.x,
+			player.movable.position.y,
+			player.shape);
+	}
+	for (pair<const uint32_t, BulletBase>& bullet_pair : bullets)
+	{
+		BulletBase&	bullet = bullet_pair.second;
+		screen.draw_point(bullet.movable.position.x,
+			bullet.movable.position.y,
+			'*');
+	}
+	screen.print();
+	cout << "direction: " << me->movable.direction << endl;
+}
--- rpg_server_test/Client.hpp
+++ rpg_server_test/Client.hpp
@@ -4,30 +4,44 @@
 #include <vector>
 #include <map>
 #include <string>
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+
 /*##############################################################################
 
 	Client
 
 ##############################################################################*/
+
+#define SCREEN_SIZE		100,27
+
 class Client
 {
+	public:
+	typedef std::chrono::system_clock	clock;
 	/*---------------------------------
 		Member
 	---------------------------------*/	
-	SocketClient			socket;
+	enum class Status
+	{
+		START,
+		LOGIN_WAIT,
+		GAME,
+		EXIT
+	};
+	
+	SocketClient					socket;
 
 	std::map<uint32_t, PlayerBase>	players;
 	std::map<uint32_t, BulletBase>	bullets;
 	PlayerBase*						me;
 	std::vector<BulletBase*>		my_bullets;
 
-	enum class Status
-	{
-		START,
-		LOGIN_WAIT,
-		GAME
-	};
-	Status					status;
+	Status							status;
+	clock::time_point				time_start;
+
+	Screen							screen;
 
 	/*---------------------------------
 		Constructor & Destructor
@@ -56,7 +70,14 @@ class Client
 	void		start();
 	void		login_wait();
 	void		game();
+	void		shot(uint32_t bullet_count);
+	void		update(double time);
 
 	//	etc				-----------------
+	double		time_now();
+	struct		PrintScreen{};
+	void		operator()(PrintScreen);
+	struct		SendMessage{};
+	void		operator()(SendMessage);
 };
 //------------------------------------------------------------------------------
